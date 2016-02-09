1338f175529d65d008b6626617c4d42e97de8b93
Parth Oza <plusparth@gmail.com>
2016-02-09 16:18:49 -0500

    Removed ConstantsBase

--- src/org/usfirst/frc/team4099/robot/subsystems/CommandBase.java
+++ src/org/usfirst/frc/team4099/robot/subsystems/CommandBase.java
@@ -6,7 +6,6 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
-import org.usfirst.frc.team4099.lib.util.ConstantsBase;
 import edu.wpi.first.wpilibj.DriverStation;
 import org.usfirst.frc.team4099.robot.OI;
 import edu.wpi.first.wpilibj.SPI;
