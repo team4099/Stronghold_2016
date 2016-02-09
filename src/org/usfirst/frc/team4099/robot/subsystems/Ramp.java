00880d4f1cfa2aea867297e62caf29e252c1f7a6
Kent Ma <tnekent@gmail.com>
2016-02-09 08:31:10 -0500

    A little bit of ramp code

--- src/org/usfirst/frc/team4099/robot/subsystems/Ramp.java
+++ src/org/usfirst/frc/team4099/robot/subsystems/Ramp.java
@@ -1,15 +1,35 @@
 package org.usfirst.frc.team4099.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team4099.lib.util.ConstantsBase;
+
 
 public class Ramp extends Subsystem {
+    private Motors motors;
+    private int LINEAR_ACTUATOR_PORT;
+    private Talon LINEAR_ACTUATOR;
 
     public Ramp() {
+        motors = new Motors();
+        LINEAR_ACTUATOR_PORT = CommandBase.constants.getInteger("LINEAR_ACTUATOR_PORT");
+
+        LINEAR_ACTUATOR = neww Talon(LINEAR_ACTUATOR_PORT);
+        motors.addMotor(LINEAR_ACTUATOR);
 
     }
 
     @Override
-    protected void initDefaultCommand() {}
+    protected void initDefaultCommand() {
+    }
+
+    public void setMotorSpeed(double speed) {
+        LINEAR_ACTUATOR.set(speed);
+    }
+
+    public void setAngle(double angle) {
+    }
+
 
 
 }
