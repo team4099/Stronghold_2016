3928e062e7f3586fda451df8cb4dda687808568e
Kent Ma <tnekent@gmail.com>
2016-02-17 17:20:50 -0500

    Working connection

--- src/org/usfirst/frc/team4099/robot/subsystems/Vision.java
+++ src/org/usfirst/frc/team4099/robot/subsystems/Vision.java
@@ -1,77 +1,49 @@
-package org.usfirst.frc.team4099.robot.subsystems;
 
 /*
  * Vision subsystem - communicates with the external processor for actual data
  */
+
+package org.usfirst.frc.team4099.robot.subsystems;
+import org.usfirst.frc.team4099.lib.util.Constants;
 import edu.wpi.first.wpilibj.command.Command;
 
-import java.util.ArrayList;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.net.HttpURLConnection;
+import java.net.URL;
 /*
  * Interface to integrate with external imaging processing 
  */
 class Vision
 {
-    /*
-    public static void main(String args[]) {
-        Vision test = new Vision(.2f);
-        test.updateVisionData();
-        System.out.printf("LA %f \nFA %f\nVA %f\n", test.getLateralAngle(), test.getFiringAcceleration(), test.getVerticalAngle());
-    }
-    */
-    private String ip = "127.0.0.1";
-    private int port = 5802;
-
     private int acquiredTarget;
     private float verticalAngle, firingAcceleration, lateralAngle;
 
-    private Socket processorSocket;
-
-    private ArrayList<Command> commandQueue;
-
-    /*
-     * Period is the update interval 
-     */
-    public Vision(float period) {
-        this.commandQueue = new ArrayList<Command>();
+    public Vision() {
+        this.acquiredTarget = false;
     }
 
     /* 
      * Everytime this command is called, method checks for new vision data from 
-     * the vision processor and sends any queued commands to the vision processor
+     * the vision processor 
      * */ 
     public void updateVisionData() {
         try {
-            this.processorSocket = new Socket(this.ip, this.port);
-            
-            BufferedReader stdout = new BufferedReader(new InputStreamReader(processorSocket.getInputStream()));
-            String serverResponse = stdout.readLine();
-            System.out.println(serverResponse);
-            String[] statArgs = serverResponse.split(",");
+            URL udoo = new URL(Constants.UDOO_RESTFUL_ENDPOINT);
+            HttpURLConnection connection = (HttpURLConnection) udoo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
 
-            /* 
-             * Can't update data if the data doesn't exist - maybe we should set
-             * the args to a null value or throw an exception when there isn't anything to shoot? 
-             */
-            if (statArgs[0].equals("0")) {
-                this.acquiredTarget = 0;
-            } else if (statArgs[0].equals("1")){
-                this.acquiredTarget = 1;
-                this.lateralAngle = Integer.parseInt(statArgs[3]);
-            } else if (statArgs[0].equals("2")) { 
-                this.acquiredTarget = 2;
-                this.verticalAngle = Integer.parseInt(statArgs[1]);
-                this.firingAcceleration = Integer.parseInt(statArgs[2]);
-            }
-
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            inputLine = in.readLine();
+            String[] angle_args = inputLine.split(",");
+            if (angle_args.length <= 1) {
+                this.acquiredTarget = false;
+            } else {
+                this.acquiredTarget = true;
+                this.lateralAngle = angle_args[0];
+                this.verticalAngle = angle_args[1];
+            in.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -103,10 +75,4 @@ class Vision
     public float getFiringAcceleration() {
         return this.firingAcceleration;
     }
-    
-    public void queueCommand(Command cmd) {
-        commandQueue.add(cmd);
-    }
 }
-
-
