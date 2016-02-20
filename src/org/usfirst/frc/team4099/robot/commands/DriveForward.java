5f457b831b585ac60ecfe98a8dcb0273c42116ee
Parth Oza <plusparth@gmail.com>
2016-02-20 12:08:52 -0500

    Added StraightThroughDefense commandgroup (just forward for now)

--- src/org/usfirst/frc/team4099/robot/commands/DriveForward.java
+++ src/org/usfirst/frc/team4099/robot/commands/DriveForward.java
@@ -6,13 +6,22 @@ import org.usfirst.frc.team4099.robot.subsystems.CommandBase;
  * Created by plato2000 on 2/17/16.
  */
 public class DriveForward extends CommandBase {
+
+    double timeout;
+
     public DriveForward() {
+        timeout = 0.01;
+        requires(driveTrain);
+    }
+
+    public DriveForward(double timeout) {
+        this.timeout = timeout;
         requires(driveTrain);
     }
 
     @Override
     protected void initialize() {
-        setTimeout(0.01);
+        setTimeout(timeout);
     }
 
     @Override
